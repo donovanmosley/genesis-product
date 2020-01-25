7a74f5def86fafabca8a1e3f550334d1ab44b174
Nicholas Hall <nicholashall@Nicholass-MacBook-Pro.local>
2020-01-25 13:50:58 -0500

    Resolved Error related to File location and Added code to fix the issue with Image displaying

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
-var book = require ('/Users/donovanmosley/Desktop/genesis-product/src/book_cover.png')
+var book = require("./tree.png");
 
-class App extends Component{
-    render(){
-        return <React.Fragment> <h1>This Page</h1>; 
-        <img src={require ('/Users/donovanmosley/Desktop/genesis-product/src/book_cover.png') }/>
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>This Page</h1>;
+        <img src={book} />
         <button>Test</button>
         <textarea>test</textarea>
-        </React.Fragment>
-    }
-
-};
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
