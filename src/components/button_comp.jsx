70bb04861448e8d891ad978d60b8958c1b6f5946
Donovan Mosley <dmosley28@gmail.com>
2020-03-10 05:28:39 -0400

    Connected to Java endpoint

--- src/components/button_comp.jsx
+++ src/components/button_comp.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+
 class Button extends Component {
   constructor(props) {
     super(props);
@@ -15,8 +16,53 @@ class Button extends Component {
 
   handleSubmit(event) {
     alert("A name was submitted: " + this.state.value);
+    fetch("http://localhost:8080/demo/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: "name=value1&email=value2"
+    })
+      .then(function() {
+        console.log("ok");
+      })
+      .catch(function() {
+        console.log("error");
+      });
+    
+    
+    
+//     fetch("http://localhost:8080/demo/add",{
+//       method: "POST",
+//       body: "name=value1&email=value2",
+//       headers: {
+//         'Content-Type': "application/x-www-form-urlencoded"
+//       },
+//     }) .then(function() {
+//       console.log("ok");
+//     })
+//     .catch(function(){
+//       console.log("error");
+//     });
+// //Make a post using fetch with our end point
+//Attach it to fields
+//Gather business requirements
+
+    // .then(response => response.json())
+    // .then(parsedJSON => console.log(parsedJSON.results))
+    // .catch(error => console.log('parsing failed', error))
+    // fetch(
+    //   "http://localhost:8080/demo/add?name=First&email=" + this.state.value,
+    //   { method: "post" }
+    // );
     event.preventDefault();
   }
+
+  // componentDidMount() {
+  //   fetch('https://localhost:8080/demo/all')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ data }));
+  // }
 
   render() {
     return (
@@ -33,7 +79,7 @@ class Button extends Component {
           <input type="submit" value="Submit" />
         </form>
 
-        {/* //* <button onClick={() => alert(document.getElementById("textinput"))}> */}
+        {/* //* <button onClick={() => alert('Hi!')}> */}
         {/* </button> */}
       </React.Fragment>
     );
